af1e89194c9bbbb7901ac28f2f1002bbe8f40a4c
RidmiVindya <vindyaridmi2002@gmail.com>
2024-12-02 19:06:17 +0530

    Worked on the landing page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 const About = () => {
-    return <h1>About Us</h1>;
+   <div className="h-screen w-full">
+     <Header />
+     <h1>About Us</h1>
+   </div>
 };
 
-export default About;
+export default About; 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,13 +16,15 @@ const Home = () => {
   //   }
   // ]
   return (
-    <div className="min-h-screen  flex flex-col">
+    <div className="h-screen w-full">
       <Header />
-      <main className="flex-grow bg-blue-200 text-center flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold text-white mb-4">Welcome to Samlox Games</h1>
-        <button className="bg-yellow-500 text-black px-6 py-2 rounded-md hover:bg-yellow-400">
-          View Portfolio
-        </button>
+      <main className="bg-blue-200 text-center h-[85vh] flex items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold text-white mb-4">Welcome to Samlox Games</h1>
+          <button className="bg-yellow-500 text-black px-6 py-2 rounded-md hover:bg-yellow-400">
+            View Portfolio
+          </button>
+        </div>
       </main>
       <Footer />
     </div>
